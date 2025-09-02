6b08b57c1f449b0b8da6201c7f161f99586bc1bc
RoadWind <3461571786@qq.com>
2025-09-02 10:46:49 +0800

    添加设置窗口的组件

---
+++ PunchPressCsharp/UI/Frm_set.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PunchPressCsharp.UI
+{
+    partial class Frm_set
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleBar = new System.Windows.Forms.Panel();
+            this.btn_close = new Sunny.UI.UIButton();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.titleBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleBar
+            // 
+            this.titleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.titleBar.Controls.Add(this.btn_close);
+            this.titleBar.Controls.Add(this.lblTitle);
+            this.titleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titleBar.Location = new System.Drawing.Point(0, 0);
+            this.titleBar.Margin = new System.Windows.Forms.Padding(0);
+            this.titleBar.Name = "titleBar";
+            this.titleBar.Size = new System.Drawing.Size(897, 40);
+            this.titleBar.TabIndex = 3;
+            // 
+            // btn_close
+            // 
+            this.btn_close.AccessibleRole = System.Windows.Forms.AccessibleRole.ButtonDropDown;
+            this.btn_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FillHoverColor = System.Drawing.Color.Red;
+            this.btn_close.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_close.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.btn_close.Location = new System.Drawing.Point(857, 0);
+            this.btn_close.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_close.Size = new System.Drawing.Size(40, 40);
+            this.btn_close.Style = Sunny.UI.UIStyle.Custom;
+            this.btn_close.TabIndex = 2;
+            this.btn_close.Text = "X";
+            this.btn_close.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(74, 21);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "模板学习";
+            // 
+            // Frm_set
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 586);
+            this.Controls.Add(this.titleBar);
+            this.Name = "Frm_set";
+            this.titleBar.ResumeLayout(false);
+            this.titleBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel titleBar;
+        private Sunny.UI.UIButton btn_close;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
