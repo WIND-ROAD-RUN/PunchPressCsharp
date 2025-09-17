80219920410254c59dd6fe90fe88026c2dd440b3
RoadWind <3461571786@qq.com>
2025-09-17 09:41:22 +0800

    添加loadShapeModel窗口

--- PunchPressCsharp/UI/Frm_loadShapeModel.Designer.cs
+++ PunchPressCsharp/UI/Frm_loadShapeModel.Designer.cs
@@ -28,20 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.titleBar = new System.Windows.Forms.Panel();
+            this.btnClose = new Sunny.UI.UIButton();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.tim_getState = new System.Windows.Forms.Timer(this.components);
+            this.titleBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // titleBar
+            // 
+            this.titleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.titleBar.Controls.Add(this.btnClose);
+            this.titleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titleBar.Location = new System.Drawing.Point(0, 0);
+            this.titleBar.Margin = new System.Windows.Forms.Padding(0);
+            this.titleBar.Name = "titleBar";
+            this.titleBar.Size = new System.Drawing.Size(1137, 40);
+            this.titleBar.TabIndex = 12;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FillHoverColor = System.Drawing.Color.Red;
+            this.btnClose.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnClose.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.btnClose.Location = new System.Drawing.Point(1097, 0);
+            this.btnClose.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnClose.Size = new System.Drawing.Size(40, 40);
+            this.btnClose.Style = Sunny.UI.UIStyle.Custom;
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "X";
+            this.btnClose.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(1137, 820);
+            this.panelContainer.TabIndex = 13;
+            // 
+            // tim_getState
+            // 
+            this.tim_getState.Enabled = true;
+            this.tim_getState.Interval = 5000;
             // 
             // Frm_loadShapeModel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(937, 732);
+            this.ClientSize = new System.Drawing.Size(1137, 820);
+            this.Controls.Add(this.titleBar);
+            this.Controls.Add(this.panelContainer);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Frm_loadShapeModel";
             this.Text = "Frm_loadShapeModel";
             this.Load += new System.EventHandler(this.Frm_loadShapeModel_Load);
+            this.titleBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel titleBar;
+        private Sunny.UI.UIButton btnClose;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Timer tim_getState;
     }
 }
